f07a44764064df2458cf9f193227be93bca7716b
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2023-09-09 03:27:18 -0600

    contrast error fix, and added gallery

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   });
   
   ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+  ScrollReveal().reveal('.news-cards', { delay: 350 });
+  ScrollReveal().reveal('.cards-banner-one', { delay: 350 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 350 });
 
 
 
